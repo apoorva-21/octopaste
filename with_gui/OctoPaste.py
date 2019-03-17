d35f7d2869edffeb400e9a3c2a8aa86087d75829
Chirag Trasikar <chiragtrasikar97@gmail.com>
2019-03-17 12:14:11 +0530

    Add files via upload

--- with_gui/OctoPaste.py
+++ with_gui/OctoPaste.py
@@ -5,12 +5,14 @@ import time
 import platform
 import clipboard
 from tkinter import *
+from tkinter import font
+import os
 
 class OctoPaste:
 
 	def __init__(self):
 		# path to the json file storing the clipboard data:
-		self.JSON_PATH = './sample.json'
+		self.JSON_PATH = './clipboard_data.json'
 
 		#max no of entries at any time in the clipboard(JSON_PATH):
 		self.MAX_ENTRIES = 10
@@ -21,35 +23,31 @@ class OctoPaste:
 		self.prev_data = ''
 		# The key combination to check
 		self.PASTE_COMBINATIONS = [
-		    {Key.ctrl, Key.alt, KeyCode(char='v')},
-		    {Key.ctrl_l, Key.alt_l, KeyCode(char='v')},
-		    {Key.ctrl_l, Key.alt_r, KeyCode(char='v')},
-		    {Key.ctrl_r, Key.alt_l, KeyCode(char='v')},
-		    {Key.ctrl_r, Key.alt_r, KeyCode(char='v')}
+		    {Key.ctrl, Key.alt, KeyCode(char='b')},
+		    {Key.ctrl_l, Key.alt_l, KeyCode(char='b')},
+		    {Key.ctrl_l, Key.alt_r, KeyCode(char='b')},
+		    {Key.ctrl_r, Key.alt_l, KeyCode(char='b')},
+		    {Key.ctrl_r, Key.alt_r, KeyCode(char='b')}
 		]
 
 		self.COPY_COMBINATIONS = [
 			{Key.ctrl, KeyCode(char = 'c')},
-			{Key.ctrl_l, Key.alt_l, KeyCode(char='c')},
-			{Key.ctrl_l, Key.alt_r, KeyCode(char='c')},
-		    {Key.ctrl_r, Key.alt_l, KeyCode(char='c')},
-		    {Key.ctrl_r, Key.alt_r, KeyCode(char='c')}
+			{Key.ctrl_l, KeyCode(char='c')},
+		    {Key.ctrl_r, KeyCode(char='c')}
 		]
 
 		self.CUT_COMBINATIONS = [
 			{Key.ctrl, KeyCode(char = 'x')},
-			{Key.ctrl_l, Key.alt_l, KeyCode(char='x')},
-			{Key.ctrl_l, Key.alt_r, KeyCode(char='x')},
-		    {Key.ctrl_r, Key.alt_l, KeyCode(char='x')},
-		    {Key.ctrl_r, Key.alt_r, KeyCode(char='x')}
+			{Key.ctrl_l, KeyCode(char='x')},
+		    {Key.ctrl_r, KeyCode(char='x')}
 		]
 
 		self.TERMINAL_PASTE_COMBINATIONS = [
-		    {Key.shift, Key.ctrl, Key.alt, KeyCode(char='V')},
-		    {Key.shift, Key.ctrl_l, Key.alt_l, KeyCode(char='V')},
-		    {Key.shift, Key.ctrl_l, Key.alt_r, KeyCode(char='V')},
-		    {Key.shift, Key.ctrl_r, Key.alt_l, KeyCode(char='V')},
-		    {Key.shift, Key.ctrl_r, Key.alt_r, KeyCode(char='V')}
+		    {Key.shift, Key.ctrl, Key.alt, KeyCode(char='B')},
+		    {Key.shift, Key.ctrl_l, Key.alt_l, KeyCode(char='B')},
+		    {Key.shift, Key.ctrl_l, Key.alt_r, KeyCode(char='B')},
+		    {Key.shift, Key.ctrl_r, Key.alt_l, KeyCode(char='B')},
+		    {Key.shift, Key.ctrl_r, Key.alt_r, KeyCode(char='B')}
 		]
 
 		self.TERMINAL_COPY_COMBINATIONS = [
@@ -66,7 +64,12 @@ class OctoPaste:
 		self.fileName = 'TestFile.txt'
 		self.top = None
 		self.Lb1 = None
-		#read from json file::
+
+		#read from json file if it exists::
+		if not os.path.exists(self.JSON_PATH):
+			with open(self.JSON_PATH, 'w') as f:
+				f.write('{"records": []}')
+
 		self.data_json = []
 		with open(self.JSON_PATH, 'rb') as f:
 			self.data_json = json.load(f)
@@ -89,11 +92,13 @@ class OctoPaste:
 			to_be_put_in_clipboard = ''
 			with open(self.JSON_PATH, 'rb') as f:
 				self.data_json = json.load(f)
+				print(self.Lb1.curselection())
+				# exit()
 				for i in self.Lb1.curselection():
 					to_be_put_in_clipboard += self.data_json['records'][i]['text']
+					# print(to_be_put_in_clipboard)
+			# exit()
 			clipboard.copy(to_be_put_in_clipboard)
-
-			print(to_be_put_in_clipboard)
 
 			# # emulate alt+tab
 			self.kb_actor.press(Key.alt)
@@ -118,43 +123,62 @@ class OctoPaste:
 			self.kb_actor.release('v')
 
 		self.top = Tk()
-		
+
 		OS = platform.system()
+
 
 		screenWidth = self.top.winfo_screenwidth()
 		screenHeight = self.top.winfo_screenheight()
-		w = 300
-		h = 160
+		w = 500
+		h = 300
+
+		octo_font = font.Font(size = int(h / 18))
+		
 		x = screenWidth - (w + 15)
 		y = (screenHeight / 2) - (h / 2)
 		self.top.geometry('%dx%d+%d+%d' % (w, h, x, y))
-		
 		if (OS == 'Windows'):
 			self.top.wm_attributes("-alpha", 0.85)
 		elif (OS == 'Linux'):
-			self.top.wait_visibility(self.top)
-			self.top.wm_attributes("-alpha", 0.85)
+			pass
+			#self.top.wait_visibility(self.top)
+			#self.top.wm_attributes("-alpha", 0.85)
 		elif (OS == 'Darwin'):
 			self.top.wait_visibility(self.top)
 			self.top.wm_attributes("-alpha", 0.85)
 		
 		scrollbar = Scrollbar(self.top, orient=VERTICAL)
-		
-		self.Lb1 = Listbox(self.top, 
-							selectmode = EXTENDED, 
-							width = w - 5, 
+		if(OS == 'Linux'):
+			self.Lb1 = Listbox(self.top, 
+								selectmode = EXTENDED,
+								width = w - 5,
+								height = h,
+								relief = SUNKEN,
+								bg = 'black',
+								fg = 'white',
+								selectbackground = '#ffffff',
+								yscrollcommand = scrollbar.set,
+								font = octo_font)
+		else:
+			self.Lb1 = Listbox(self.top, 
+							selectmode = EXTENDED,
+							width = w - 5,
 							height = h,
 							relief = SUNKEN,
 							bg = 'black',
 							fg = 'white',
 							selectbackground = '#262523',
-							yscrollcommand = scrollbar.set)
+							yscrollcommand = scrollbar.set,
+							font = octo_font)
 		
 
 		with open(self.JSON_PATH, 'rb') as f:
 			self.data_json = json.load(f)
 			for index, element in enumerate(self.data_json['records']):
-				self.Lb1.insert(index, element['text'][:self.MAX_TEXT_LEN])
+				if len(element['text']) <= self.MAX_TEXT_LEN:
+					self.Lb1.insert(index, str(index + 1) + ') '+ element['text'][:self.MAX_TEXT_LEN])
+				else: 
+					self.Lb1.insert(index, str(index + 1) + ') '+ element['text'][:int(self.MAX_TEXT_LEN / 2 - 1)] + ' ... ' + element['text'][int(-self.MAX_TEXT_LEN / 2 - 2):])
 
 		self.Lb1.bind("<Return>", return_pressed)
 	
@@ -172,7 +196,7 @@ class OctoPaste:
 
 		with open(self.JSON_PATH, 'w') as f:
 			json.dump(self.data_json, f)
-		print('Dumped the new shit!')
+		###print('Dumped the new shit!')
 
 
 	def execute_paste(self, is_terminal = False):
@@ -184,18 +208,27 @@ class OctoPaste:
 
 
 	def execute_copy(self):
+		time.sleep(0.1)
 		copied_data = clipboard.paste()
 
 		if not copied_data == self.prev_data: 
 			self.write_to_json(copied_data)
 			self.prev_data = copied_data
 
+		# Update dropdown
+		# if self.Lb1:
+		# 	self.Lb1.Items.Clear()
+		# 	with open(JSON_PATH, 'rb') as f:
+		# 		data_json = json.load(f)
+		# 		for index, element in enumerate(data_json['records']):
+		# 			self.Lb1.insert(index, element['text'][:MAX_TEXT_LEN])
+
 
 	# def execute(self):
 	# 	self.current.clear()
 	# 	#readFile(fileName)
 	# 	self.dropDown()
-	# 	#print(buffer['One'])
+	# 	####print(buffer['One'])
 
 
 	# def readFile(self, fileName):
@@ -204,24 +237,24 @@ class OctoPaste:
 
 
 	def on_press(self, key):
-		if any([key in COMBO for COMBO in self.PASTE_COMBINATIONS]):
-			self.current.add(key)
-			if any(all(k in self.current for k in COMBO) for COMBO in self.PASTE_COMBINATIONS):
-				print('ctrlV detected!')
-				# print(current)
-				self.execute_paste(is_terminal = False)
-
 		if any([key in COMBO for COMBO in self.TERMINAL_PASTE_COMBINATIONS]):
 			self.current.add(key)
 			if any(all(k in self.current for k in COMBO) for COMBO in self.TERMINAL_PASTE_COMBINATIONS):
-				print('ctrlShiftV detected!')
-				# print(current)
+				###print('ctrlShiftV detected!')
+				# ###print(current)
 				self.execute_paste(is_terminal = True)
+				
+		if any([key in COMBO for COMBO in self.PASTE_COMBINATIONS]):
+			self.current.add(key)
+			if any(all(k in self.current for k in COMBO) for COMBO in self.PASTE_COMBINATIONS):
+				###print('ctrlV detected!')
+				# ##print(current)
+				self.execute_paste(is_terminal = False)
 
 		if any([key in COMBO for COMBO in self.COPY_COMBINATIONS]):
 			self.current.add(key)
 			if any(all(k in self.current for k in COMBO) for COMBO in self.COPY_COMBINATIONS):
-				print('ctrlC detected!')
+				##print('ctrlC detected!')
 				self.execute_copy()
 				#empty the current set, since task has been executed now
 				self.current.clear()
@@ -230,7 +263,7 @@ class OctoPaste:
 		if any([key in COMBO for COMBO in self.TERMINAL_COPY_COMBINATIONS]):
 			self.current.add(key)
 			if any(all(k in self.current for k in COMBO) for COMBO in self.TERMINAL_COPY_COMBINATIONS):
-				print('ctrlShiftC detected!')
+				##print('ctrlShiftC detected!')
 				self.execute_copy()
 				#empty the current set, since task has been executed now
 				self.current.clear()
@@ -238,7 +271,7 @@ class OctoPaste:
 		if any([key in COMBO for COMBO in self.CUT_COMBINATIONS]):
 			self.current.add(key)
 			if any(all(k in self.current for k in COMBO) for COMBO in self.CUT_COMBINATIONS):
-				print('ctrlX detected!')
+				##print('ctrlX detected!')
 				self.execute_copy()
 				#empty the current set, since task has been executed now
 				self.current.clear()
